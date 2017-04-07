a1d750a6326f21140c15f4074d4d4929c8a25de1
PuChen7 <puchen8295@gmail.com>
2017-04-07 00:14:35 -0400

    added several onClick functions

--- calculator/app/src/main/java/com/example/pu/firstclaculator/MainActivity.java
+++ calculator/app/src/main/java/com/example/pu/firstclaculator/MainActivity.java
@@ -3,23 +3,61 @@ package com.example.pu.firstclaculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // set up buttons
+    private Button one, two, three, four, five, six, seven, eight, nine, zero,
+            add, subtract, divide, multiply,
+            clear, mod, dot, double_zero, equal;
+
+    private TextView screen;
+    private String display = "";
+    private String operator;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        screen = (TextView)findViewById(R.id.textView);
+        screen.setText(display);
     }
 
-    // two input number
-    private double numbOne, numTwo;
+    private void updateDisplay(){
+        screen.setText(display);
+    }
 
-    // operators
-    private static final char ADDITION = '+';
-    private static final char MULTIPLICATION = '*';
-    private static final char DIVISION = '/';
-    private static final char SUBTRACTOIN = '-';
-    
+    public void onClickNumber(View view){
+        Button button = (Button) view;
+        display = display + button.getText();
+        updateDisplay();
+    }
+
+    public void onClickOperator(View view){
+        Button button = (Button) view;
+        display = display + button.getText();
+        operator = button.getText().toString();
+        updateDisplay();
+    }
+
+    private void clear(){
+        display = "";
+        operator = "";
+        updateDisplay();
+    }
+
+    public void onClickEqual(View view){
+
+    }
+
+    public void onClickClear(View view){
+        clear();
+        updateDisplay();
+    }
+
+    public void onClickDot(View view){
+
+    }
 }
