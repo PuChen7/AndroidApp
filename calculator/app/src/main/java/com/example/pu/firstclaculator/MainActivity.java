b34673499351d6a6e24488b14f174577400f4bc8
Pu Chen <puchen8295@gmail.com>
2017-04-07 23:32:42 -0400

    Update MainActivity.java

--- calculator/app/src/main/java/com/example/pu/firstclaculator/MainActivity.java
+++ calculator/app/src/main/java/com/example/pu/firstclaculator/MainActivity.java
@@ -5,17 +5,15 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.widget.TextView;
+import android.util.Log;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-    // set up buttons
-    private Button one, two, three, four, five, six, seven, eight, nine, zero,
-            add, subtract, divide, multiply,
-            clear, mod, dot, double_zero, equal;
-
     private TextView screen;
     private String display = "";
     private String operator;
-
+    private String result = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +24,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateDisplay(){
-        screen.setText(display);
+       screen.setText(display);
     }
 
     public void onClickNumber(View view){
+        if (result != ""){
+            clear();
+            updateDisplay();
+        }
         Button button = (Button) view;
-        display = display + button.getText();
+        display +=button.getText();
         updateDisplay();
     }
 
     public void onClickOperator(View view){
+        if (result != ""){
+            display = result;
+            result = "";
+        }
         Button button = (Button) view;
-        display = display + button.getText();
+        display += button.getText();
         operator = button.getText().toString();
         updateDisplay();
     }
@@ -45,11 +51,7 @@ public class MainActivity extends AppCompatActivity {
     private void clear(){
         display = "";
         operator = "";
-        updateDisplay();
-    }
-
-    public void onClickEqual(View view){
-
+        result = "";
     }
 
     public void onClickClear(View view){
@@ -59,5 +61,39 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickDot(View view){
 
+    }
+
+    private double calculate(String num1, String num2, String operator){
+        switch (operator){
+            case "+":
+                return Double.valueOf(num1) + Double.valueOf(num2);
+            case "-":
+                return Double.valueOf(num1) - Double.valueOf(num2);
+            case "*":
+                return Double.valueOf(num1) * Double.valueOf(num2);
+            case "%":
+                return Double.valueOf(num1) % Double.valueOf(num2);
+            case "/":
+                try{
+                    return Double.valueOf(num1) / Double.valueOf(num2);
+                } catch(Exception e){
+                    Log.d("Calc", e.getMessage());
+                }
+            default:
+                return -1;
+        }
+    }
+
+    private void getResult(String[] components){
+
+    }
+
+    public void onClickEqual(View view) {
+        String[] components = display.split(Pattern.quote(operator));
+        if (components.length < 2){
+            return;
+        }
+        result = String.valueOf(calculate(components[0], components[1], operator));
+        screen.setText(display + "\n" + String.valueOf(result));
     }
 }
